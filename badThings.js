df40c62438ca45c3c00ace726dc9ef7262abdb65
mir592 <miriam5592@gmail.com>
2017-08-28 17:37:11 +0200

    aparecen puntitos aleatoriamente

--- badThings.js
+++ badThings.js
@@ -1,7 +1,29 @@
-function BadThins(){
-  this.element = $('<div>').attr('class',bads);
+function BadThings(){
+  this.element = $('<div>').addClass('bads');
+  this.speedX = 10;
+  this.nextPx = 0;
+  this.posX = 0;
+  this.posY = 0;
+  $('.board').append(this.element);
+  console.log("HOLI");
 }
 
-BadThins.prototype.moveDown = function(){
-  
+BadThings.prototype.positions = function() {
+  this.posX = parseInt((this.element).position().left);
+  this.posY = parseInt($(this.element).position().top);
+  this.width = parseInt($(this.element).css('width'));
+  console.log("Posiciones", this.posX, this.posY, this.width);
+};
+
+BadThings.prototype.appearTop = function(){
+  this.nextPx = parseInt(Math.floor(Math.random()*($('.board').width())));
+  console.log("appearTop", this.nextPx);
+
+};
+
+BadThings.prototype.goDown = function(speed){
+  console.log("GODOWN");
+//this.posY += this.speedX;
+  //console.log(this.posY += this.speedX);
+  this.element.css({top:this.posY, left:this.nextPx });
 };
--- index.js
+++ index.js
@@ -2,17 +2,23 @@ var board = new Board();
 var player1 = new Player('.p1');
 var player2 = new Player('.p2');
 
+
 $(document).keydown(function() {
+
   if(event.keyCode === 37){
     player1.left();}
   if(event.keyCode === 39) {
     player1.right();}
-  if(event.keyCode === 65){
+  if(event.keyCode === 65){ // tecla A
       player2.left();}
-  if(event.keyCode === 68) {
+  if(event.keyCode === 68) { // tecla D
       player2.right();}
-});
+    });
+
 $(document).on('click', function(){
-
-
+  setInterval(function(){
+  var bads = new BadThings();
+  bads.appearTop();
+  bads.goDown();
+},2000);
 });
